d9f38dd57448c6d5031e4d5f7464ae5b9310dca2
aad13 <adam.lounes@yahoo.fr>
2019-12-05 16:37:22 +0100

    Added local project to repository.

---
+++ src/test/java/com/adaming/personalprojectal/Covaad13/Covaad13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaming.personalprojectal.Covaad13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covaad13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
