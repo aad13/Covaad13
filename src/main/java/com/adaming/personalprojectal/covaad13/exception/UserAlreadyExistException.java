11a8747a1664158a2f66f79a1b69520b429769e1
aad13 <adam.lounes@yahoo.fr>
2019-12-06 17:41:24 +0100

    "Added "ou le numéro de téléphone" to log message in default constructor of UserAlreadyExistException."

--- src/main/java/com/adaming/personalprojectal/covaad13/exception/UserAlreadyExistException.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/exception/UserAlreadyExistException.java
@@ -3,6 +3,6 @@ package com.adaming.personalprojectal.covaad13.exception;
 public class UserAlreadyExistException extends Exception {
 
     public UserAlreadyExistException(){
-        System.out.println("!!!L'e-mail que vous avez renseigné existe déjà.!!!");
+        System.out.println("!!!L'e-mail ou le numéro de téléphone que vous avez renseigné existe déjà.!!!");
     }
 }
