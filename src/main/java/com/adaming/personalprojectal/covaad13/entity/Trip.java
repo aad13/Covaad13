4043bec54d0f45cec51fa5473c541f777bca0fc6
aad13 <adam.lounes@yahoo.fr>
2019-12-06 14:12:05 +0100

    "Filled Trip and User entities with annotations, fields, getters and setters and default constructor."

--- src/main/java/com/adaming/personalprojectal/covaad13/entity/Trip.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/entity/Trip.java
@@ -1,9 +1,89 @@
 package com.adaming.personalprojectal.covaad13.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "trips")
 public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private float price;
+    private String departureS;
+    private String arrivalS;
+    private LocalDateTime departureT;
+    private int nbPlacesLeft;
+    private boolean over;
+    @ManyToMany(mappedBy = "trips")
+    private List<User> passengers;
+
+    public Trip() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDepartureS() {
+        return departureS;
+    }
+
+    public void setDepartureS(String departureS) {
+        this.departureS = departureS;
+    }
+
+    public String getArrivalS() {
+        return arrivalS;
+    }
+
+    public void setArrivalS(String arrivalS) {
+        this.arrivalS = arrivalS;
+    }
+
+    public LocalDateTime getDepartureT() {
+        return departureT;
+    }
+
+    public void setDepartureT(LocalDateTime departureT) {
+        this.departureT = departureT;
+    }
+
+    public int getNbPlacesLeft() {
+        return nbPlacesLeft;
+    }
+
+    public void setNbPlacesLeft(int nbPlacesLeft) {
+        this.nbPlacesLeft = nbPlacesLeft;
+    }
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public List<User> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<User> passengers) {
+        this.passengers = passengers;
+    }
 }
--- src/main/java/com/adaming/personalprojectal/covaad13/entity/User.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/entity/User.java
@@ -1,9 +1,98 @@
 package com.adaming.personalprojectal.covaad13.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String vehicle;
+    private int nbPlaces;
+    private boolean owner;
+    @ManyToMany
+    @JoinTable(name = "user_trip", joinColumns = @JoinColumn(name="user_id"),inverseJoinColumns = @JoinColumn(name="course_id"))
+    private List<Trip> trips;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public int getNbPlaces() {
+        return nbPlaces;
+    }
+
+    public void setNbPlaces(int nbPlaces) {
+        this.nbPlaces = nbPlaces;
+    }
+
+    public boolean isOwner() {
+        return owner;
+    }
+
+    public void setOwner(boolean owner) {
+        this.owner = owner;
+    }
+
+    public List<Trip> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(List<Trip> trips) {
+        this.trips = trips;
+    }
 }
