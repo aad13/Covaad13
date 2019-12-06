4ca66372a3bb81bdc25c76e752082d60e60f7a3d
aad13 <adam.lounes@yahoo.fr>
2019-12-06 14:16:28 +0100

    "Added UserRestController and TripRestController classes with annotations and fields."

---
+++ src/main/java/com/adaming/personalprojectal/covaad13/restcontroller/UserRestController.java
@@ -0,0 +1,17 @@
+package com.adaming.personalprojectal.covaad13.restcontroller;
+
+import com.adaming.personalprojectal.covaad13.service.TripService;
+import com.adaming.personalprojectal.covaad13.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/covaad/webapi")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TripService tripService;
+}
