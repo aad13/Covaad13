3a0f1b5f0ad02c1dfb852fb105bc2ecd3a2fc6c0
aad13 <adam.lounes@yahoo.fr>
2019-12-08 11:31:56 +0100

    "Added method getUserByEmailOrPhoneNumber with @Query in UserRepository."

--- src/main/java/com/adaming/personalprojectal/covaad13/repository/UserRepository.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.adaming.personalprojectal.covaad13.repository;
 
 import com.adaming.personalprojectal.covaad13.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User,Long> {
+
+    @Query("SELECT u FROM User AS u WHERE u.email=:email OR u.phoneNumber=:phoneNumber")
+    User getUserByEmailPOrPhoneNumber(String email, String phoneNumber);
 }
