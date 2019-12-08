6aa17b645e09baa09071184373d4ae75d5b4537d
aad13 <adam.lounes@yahoo.fr>
2019-12-08 11:33:26 +0100

    "Added method getTripByOwnerAndDepartureSAAndArrivalSAndDepartureT with @Query in UserRepository."

--- src/main/java/com/adaming/personalprojectal/covaad13/repository/TripRepository.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/repository/TripRepository.java
@@ -1,9 +1,16 @@
 package com.adaming.personalprojectal.covaad13.repository;
 
 import com.adaming.personalprojectal.covaad13.entity.Trip;
+import com.adaming.personalprojectal.covaad13.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface TripRepository extends CrudRepository<Trip,Long> {
+
+    @Query("SELECT t FROM Trip AS t WHERE t.owner=:owner AND t.departureS=:departureS AND t.arrivalS=:arrivalS AND t.departureT=:departureT")
+    Trip getTripByOwnerAndDepartureSAAndArrivalSAndDepartureT(User owner, String departureS, String arrivalS, LocalDateTime departureT);
 }
