5ee56311a839e5a711cb020616520593aa28d914
aad13 <adam.lounes@yahoo.fr>
2019-12-08 11:35:05 +0100

    "Added methods register with @Transactional, fetchById, fetchAll in UserService."

--- src/main/java/com/adaming/personalprojectal/covaad13/service/UserService.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/service/UserService.java
@@ -1,12 +1,38 @@
 package com.adaming.personalprojectal.covaad13.service;
 
+import com.adaming.personalprojectal.covaad13.entity.User;
+import com.adaming.personalprojectal.covaad13.exception.NullUserException;
+import com.adaming.personalprojectal.covaad13.exception.UserAlreadyExistException;
 import com.adaming.personalprojectal.covaad13.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class UserService{
 
     @Autowired
     private UserRepository userRepository;
+
+    @Transactional
+    public void register(User u) throws NullUserException, UserAlreadyExistException {
+        if (u==null){
+            throw new NullUserException();
+        }else{
+            if(this.userRepository.getUserByEmailPOrPhoneNumber(u.getEmail(),u.getPhoneNumber())==null){
+                this.userRepository.save(u);
+            }else{
+                throw new UserAlreadyExistException();
+            }
+        }
+    }
+
+    public User fetchById(Long id){
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public Iterable<User> fetchAll(){
+        return this.userRepository.findAll();
+    }
 }
