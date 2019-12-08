541b9ed291ae310efcaec395b5e18548cbf9b287
aad13 <adam.lounes@yahoo.fr>
2019-12-08 11:41:24 +0100

    "Added code to initialize database in Application file."

--- src/main/java/com/adaming/personalprojectal/covaad13/Covaad13Application.java
+++ src/main/java/com/adaming/personalprojectal/covaad13/Covaad13Application.java
@@ -1,8 +1,18 @@
 package com.adaming.personalprojectal.covaad13;
 
+import com.adaming.personalprojectal.covaad13.entity.Trip;
+import com.adaming.personalprojectal.covaad13.entity.User;
+import com.adaming.personalprojectal.covaad13.exception.*;
+import com.adaming.personalprojectal.covaad13.service.TripService;
+import com.adaming.personalprojectal.covaad13.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Covaad13Application implements CommandLineRunner {
@@ -11,8 +21,39 @@ public class Covaad13Application implements CommandLineRunner {
 		SpringApplication.run(Covaad13Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private TripService tripService;
 
+	@Override
+	public void run(String... args) {
+		User u1 = new User("Adel","ABARE","adel.abare@gmail.com","0654328990","Peugeot",3);
+		User u2 = new User("Boris","BARRINO","boris.barrino@gmail.com","0654328991","Renault",4);
+		User u3 = new User("Céline","CAZONA","celine.cazona@gmail.com","0654328992","Ford",4);
+		User u4 = new User("Damien","DEMURET","damien.demuret@gmail.com","0654328993","Aucun",0);
+
+		for (User u:new ArrayList<>(Arrays.asList(u1,u2,u3,u4))){
+			try {
+				this.userService.register(u);
+			} catch (NullUserException | UserAlreadyExistException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Trip t11 = new Trip(15,"Nice","Aix",LocalDateTime.of(2020,12,6,13,50),u1.getNbPlaces(),false,u1,new ArrayList<>(Arrays.asList(u2,u3,u4)));
+		Trip t12 = new Trip(25,"Toulon","Lyon",LocalDateTime.of(2020,9,2,9,15),u1.getNbPlaces(),false,u1,new ArrayList<>(Arrays.asList(u2,u4)));
+		Trip t13 = new Trip(27.50f,"Montpellier","Toulouse",LocalDateTime.of(2020,4,13,21,10),u1.getNbPlaces(),false,u1,new ArrayList<>(Arrays.asList(u2,u3)));
+		Trip t21 = new Trip(10,"Antibes","Aix",LocalDateTime.of(2020,8,23,18,0),u2.getNbPlaces(),false,u2,new ArrayList<>(Arrays.asList(u3,u4)));
+		Trip t22 = new Trip(20,"Antibes","Béziers",LocalDateTime.of(2020,6,15,10,30),u2.getNbPlaces(),false,u2,new ArrayList<>(Arrays.asList(u1,u3,u4)));
+		Trip t31 = new Trip(7.50f,"Aix","Marseille",LocalDateTime.of(2021,3,12,13,30),u3.getNbPlaces(),false,u3,new ArrayList<>(Arrays.asList(u2,u4)));
+
+		for (Trip t:new ArrayList<>(Arrays.asList(t11,t12,t13,t21,t22,t31))){
+			try {
+				this.tripService.register(t);
+			} catch (NullTripException | TripAlreadyExistException | TooEarlyTripException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
